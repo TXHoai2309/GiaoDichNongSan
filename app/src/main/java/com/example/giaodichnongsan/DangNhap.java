638ee12528f9b6b2dc386c9ff8f2bf9d471fca31
ZedoKen <tdkzedo@gmail.com>
2026-04-15 13:05:53 +0700

    Chung login

---
+++ app/src/main/java/com/example/giaodichnongsan/DangNhap.java
@@ -0,0 +1,13 @@
+package com.example.giaodichnongsan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DangNhap extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+    }
+}
