0dfdfdace7ea20f0ac3863a9900968b844573586
ZedoKen <tdkzedo@gmail.com>
2026-04-16 22:10:17 +0700

    firebase connecting còn lỗi

--- app/src/main/java/com/example/giaodichnongsan/MainActivity.java
+++ app/src/main/java/com/example/giaodichnongsan/MainActivity.java
@@ -9,11 +9,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.frameLayout, fragment)
                 .commit();
     }
+
 }
